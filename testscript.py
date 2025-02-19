083c96f18a15ea88421398a8e0bc82c06c2505ad
Nyagol <nyagolpeter@gmail.com>
2025-02-19 19:37:27 +0300

    Initial commit

---
+++ testscript.py
@@ -0,0 +1,24 @@
+#Integer
+a = 2
+print(a)
+
+#Float
+pi = 3.14
+print(pi)
+
+#string
+name = "Peter Nyagol"
+print(name)
+
+#Boolean
+x = True
+print(x)
+print(type(a))
+
+#It is important to note that variable assignments works from LEFT to RIGH henace 0 = k will always give a syntax error
+
+
+r, t, p = 9, 20, 32
+
+print(p)
+print(t,r)
