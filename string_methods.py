9f6242ad8865b87cf40689f50f87f7649d1523e8
Nyagol <nyagolpeter@gmail.com>
2025-02-22 04:14:57 +0300

    Updated string_methods.py with new code

---
+++ string_methods.py
@@ -0,0 +1,11 @@
+course = 'Python for beginners'
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course)
+print(course.find("f"))
+print(course.replace("for", "is Dificult for"))
+print("for" in course)
+
+# the find method returns the index
+# the in operator returns a boolean whether of not what we want is present in the string
