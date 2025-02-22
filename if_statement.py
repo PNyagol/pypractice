2841ee185cde723a9db60155a9ce5414fb05cbfd
Nyagol <nyagolpeter@gmail.com>
2025-02-22 04:53:24 +0300

    changes added

--- if_statement.py
+++ if_statement.py
@@ -1,12 +1,27 @@
-is_hot = True
+is_hot = False
+is_cold = False
 
 if is_hot:
     print("It's a hot day")
     print("Drink plenty of water")
 
 
-else:
+elif is_cold:
     print("It's a cold day")
     print("Wear warm cloths")
 
-print("Enjoy your day")
+else:
+    print("Enjoy your day")
+
+# Test
+
+price = 1000000
+has_good_credit = True
+
+if has_good_credit:
+    down_payment = 0.1 * price
+
+else:
+    down_payment = 0.2 * price
+
+print(f"Down Payment = ${down_payment}")
