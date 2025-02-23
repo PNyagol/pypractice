41e048aca5201c76030581e76ea893d12f643637
Nyagol <nyagolpeter@gmail.com>
2025-02-23 22:15:32 +0300

    more code

---
+++ list.py
@@ -0,0 +1,21 @@
+names = ['John', 'Ohas', 'Peter', 'Joan']
+names[1] = 'Helen'
+
+print(names)
+print(names [3])
+print(names [-2])
+print(names [1:])
+print(names [1:3])
+print(names [:])
+
+#Find the largest number in a list
+
+numbers = [20, 67, 3, 6, 2, 8, 4, 10]
+max = numbers [0]
+for number in numbers:
+    if number > max:
+        max = number
+print(max)
+
+
+# 2D Lists
